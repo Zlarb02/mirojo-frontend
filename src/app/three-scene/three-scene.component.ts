072d37239ec34ae638819db2427d75a311de741a
unknown <pogoda.etienne@outlook.fr>
2025-01-28 10:01:03 +0100

    add .js at GLTFLoader import

--- src/app/three-scene/three-scene.component.ts
+++ src/app/three-scene/three-scene.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, HostListener } from "@angular/core";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 @Component({
