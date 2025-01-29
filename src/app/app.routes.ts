dcd425bf82c05ff1e7a10a681cd9e879bb884e70
Etienne Pogoda <pogoda.etienne@outlook.fr>
2025-01-29 18:20:58 +0100

    display mail sender & roll dice online for production test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
-import { Routes } from "@angular/router";
-import { WelcomeComponent } from "./welcome/welcome.component";
-import { LoginComponent } from "./login/login.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { DiceRollComponent } from "./dice-roll/dice-roll.component";
-import { AuthComponent } from "./auth/auth.component";
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DiceRollComponent } from './dice-roll/dice-roll.component';
+import { AuthComponent } from './auth/auth.component';
 
 export const routes: Routes = [
-  { path: "welcome", component: WelcomeComponent },
-  { path: "login", component: AuthComponent },
-  { path: "profile", component: ProfileComponent },
-  { path: "dice-roll", component: DiceRollComponent },
-  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: AuthComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'dice-roll', component: DiceRollComponent },
+  { path: '', redirectTo: '/dice-roll', pathMatch: 'full' },
 ];
