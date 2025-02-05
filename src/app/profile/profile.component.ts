3cf5cb43cf2d63dd704080e86de4eabeb121eaaa
Etienne Pogoda <pogoda.etienne@outlook.fr>
2025-02-05 01:04:45 +0100

    edit profile upgrade and search for styles improvements

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,13 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { Router } from "@angular/router";
-import { FooterComponent } from "../footer/footer.component";
-import { AuthService } from "../services/auth.service";
-import { NavComponent } from "../nav/nav.component";
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { AuthService } from '../services/auth.service';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
-  selector: "app-profile",
+  selector: 'app-profile',
   standalone: true,
   imports: [
     CommonModule,
@@ -15,49 +15,114 @@ import { NavComponent } from "../nav/nav.component";
     FooterComponent,
     NavComponent,
     FormsModule,
-  ], // Ajout de ReactiveFormsModule et AvatarComponent
-  templateUrl: "./profile.component.html",
-  styleUrls: ["./profile.component.css"],
+  ],
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
   authService = inject(AuthService);
   router = inject(Router);
-  newUsername = "";
-  newFullName = "";
-  newAvatarUrl = "";
-  newWebsite = "";
+
+  // Variables pour stocker les nouvelles valeurs saisies
+  newUsername = '';
+  newFullName = '';
+  newAvatarUrl = '';
+  newWebsite = '';
+
+  // Flags pour indiquer si chaque champ est en mode édition
+  editUsername = false;
+  editFullName = false;
+  editAvatar = false;
+  editWebsite = false;
 
   logout(): void {
     this.authService.logout().then(() => {
-      this.router.navigateByUrl("/login");
+      this.router.navigateByUrl('/login');
     });
   }
 
+  // Démarrer l'édition d'un champ donné et initialiser la valeur de l'input
+  startEdit(field: string): void {
+    switch (field) {
+      case 'username':
+        this.newUsername = this.authService.currentUser()?.username || '';
+        this.editUsername = true;
+        break;
+      case 'fullName':
+        this.newFullName = this.authService.currentUser()?.full_name || '';
+        this.editFullName = true;
+        break;
+      case 'website':
+        this.newWebsite = this.authService.currentUser()?.website || '';
+        this.editWebsite = true;
+        break;
+      case 'avatar':
+        this.newAvatarUrl = this.authService.currentUser()?.avatar_url || '';
+        this.editAvatar = true;
+        break;
+    }
+  }
+
+  // Méthodes de mise à jour
   updateUsername() {
     this.authService.updateUsername(this.newUsername).subscribe({
-      next: () => alert("Nom d’utilisateur mis à jour !"),
-      error: (err) => alert("Erreur : " + err.message),
+      next: () => {
+        alert('Nom d’utilisateur mis à jour !');
+        this.editUsername = false;
+      },
+      error: (err) => alert('Erreur : ' + err.message),
     });
   }
 
   updateFullName() {
     this.authService.updateFullName(this.newFullName).subscribe({
-      next: () => alert("Nom complet mis à jour !"),
-      error: (err) => alert("Erreur : " + err.message),
+      next: () => {
+        alert('Nom complet mis à jour !');
+        this.editFullName = false;
+      },
+      error: (err) => alert('Erreur : ' + err.message),
     });
   }
 
   updateAvatarUrl() {
     this.authService.updateAvatarUrl(this.newAvatarUrl).subscribe({
-      next: () => alert("URL de l’avatar mise à jour !"),
-      error: (err) => alert("Erreur : " + err.message),
+      next: () => {
+        alert('URL de l’avatar mise à jour !');
+        this.editAvatar = false;
+      },
+      error: (err) => alert('Erreur : ' + err.message),
     });
   }
 
   updateWebsite() {
     this.authService.updateWebsite(this.newWebsite).subscribe({
-      next: () => alert("Site web mis à jour !"),
-      error: (err) => alert("Erreur : " + err.message),
+      next: () => {
+        alert('Site web mis à jour !');
+        this.editWebsite = false;
+      },
+      error: (err) => alert('Erreur : ' + err.message),
     });
+  }
+
+  // Annuler l'édition et réinitialiser l'input à la valeur actuelle
+  cancelEdit(field: string) {
+    switch (field) {
+      case 'username':
+        this.newUsername = this.authService.currentUser()?.username || '';
+        this.editUsername = false;
+        break;
+      case 'fullName':
+        this.newFullName = this.authService.currentUser()?.full_name || '';
+        this.editFullName = false;
+        break;
+      case 'website':
+        this.newWebsite = this.authService.currentUser()?.website || '';
+        this.editWebsite = false;
+        break;
+      case 'avatar':
+        this.newAvatarUrl = this.authService.currentUser()?.avatar_url || '';
+        this.editAvatar = false;
+        break;
+    }
   }
 }
